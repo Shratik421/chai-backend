c0ac694e98a790a29d3f4d3dd2714aa1cfceab37
Shratik421 <Shratikpunde@gmail.com>
2024-01-11 07:52:29 +0530

    models added

---
+++ src/models/playlist.model.js
@@ -0,0 +1,26 @@
+import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const playListSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    videoes: {
+      type: Schema.Types.ObjectId,
+      ref: "Video",
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+export const Playlist = mongoose.model("playlist", playListSchema);
